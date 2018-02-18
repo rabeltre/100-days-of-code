6143f4de660450bc481f21456b0882fb6cf38498
Rafael Beltre <rabeltre@outlook.com>
2018-02-18 02:09:09 -0400

    I have done some Advance working with nodejs and mongodb

--- projects/NodeExpressMongoDb/server.js
+++ projects/NodeExpressMongoDb/server.js
@@ -4,24 +4,37 @@ const app  = express();
 const bodyParser= require('body-parser');
 
 
-const MongoClient = require('mongodb').MongoClient
+const MongoClient= require('mongodb').MongoClient
 
-MongoClient.connect('link-to-mongodb', function(err, database){
+var db
 
-})
-
-
-app.listen(3000, function () {
+MongoClient.connect('mongodb://rabeltre:rabeltre@ds111648.mlab.com:11648/rabeltre', function(err, database){
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, function(){
     console.log('listening on 3000')
+  })
 })
+
 
 app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', function (req, res) {
     // res.send('Hello word')
   res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/quotes', function (req, res) {
-    console.log(req.body)
-    console.log('Helloooooooooooooooooo')
+    db.collection('quotes').insert(req.body, function (err, result) {
+        if (err) return console.log(err)
+
+        console.log('saved to database')
+        res.redirect('/')
+    })
 });
+
+app.get('/getQuotes', function (req, res) {
+    db.collection('quotes').find().toArray(function (err, result) {
+        res.send(result);
+    })
+})

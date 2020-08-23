45111d063b5374025aa687538a7ee01d0d093102
Rafael Beltre <rbeltrecastro@gmail.com>
2020-08-23 16:48:42 -0400

    update

--- projects/PythonScripts/General_purpose/django_example/myside/core/admin.py
+++ projects/PythonScripts/General_purpose/django_example/myside/core/admin.py
@@ -1,17 +1,46 @@
 from django.contrib import admin
 from core.models import Person, Course, Grade
+from django.db.models import Avg
+from django.utils.html import format_html
+from django.urls import reverse
+from django.utils.http import urlencode
 
 # Register your models here.
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name')
+    list_display = ('last_name', 'first_name', 'show_average')
+    search_fields = ("last_name__startswith", )
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields["first_name"].label = "Firts Name (Humans only!):"
+        return form
+
+    def show_average(self, obj):
+        result = Grade.objects.filter(person=obj).aggregate(Avg("grade"))
+#        return result["grade__avg"]
+        return format_html("<b><i>{}</i></b>", result["grade__avg"])
+
+    show_average.short_description = "Average"
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "year", "view_students_link")
+    list_filter = ("year", )
+
+    def view_students_link(self, obj):
+        count = obj.person_set.count()
+        url = (
+            reverse("admin:core_person_changelist")
+            + "?"
+            + urlencode({"courses__id": f"{obj.id}"})
+        )
+        return format_html('<a href="{}"> {} Students</a>', url, count)
+
+    view_students_link.short_description = "Students"
 
 
 @admin.register(Grade)

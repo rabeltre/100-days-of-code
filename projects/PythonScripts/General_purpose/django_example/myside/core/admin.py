1ccfe42271fc5405d5263dc41a2e1e83fd0159d0
Rafael Beltre <rbeltrecastro@gmail.com>
2020-08-23 13:48:26 -0400

    update

--- projects/PythonScripts/General_purpose/django_example/myside/core/admin.py
+++ projects/PythonScripts/General_purpose/django_example/myside/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from core.models import Person, Course, Grade
 
 # Register your models here.
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name')
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    pass
--- projects/PythonScripts/General_purpose/django_example/myside/core/models.py
+++ projects/PythonScripts/General_purpose/django_example/myside/core/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+
+class Person(models.Model):
+    last_name = models.TextField()
+    first_name = models.TextField()
+    course = models.ManyToManyField("Course", blank=True)
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
+
+    class Meta:
+        verbose_name_plural = 'People'
+        ordering = ('last_name', 'first_name' )
+
+
+class Course(models.Model):
+    name = models.TextField()
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}, {self.year}"
+
+    class Meta:
+        unique_together = ("name", "year")
+
+
+class Grade(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    grade = models.PositiveIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)])
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.person.__str__()} --> {self.grade} --> {self.course.__str__()}"
+
+
